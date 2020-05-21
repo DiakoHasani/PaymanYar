bcf1ff5dac9cf0804344cba25a76155bc01e7c7c
shahab <comutershahab67@gmail.com>
2020-05-21 15:25:06 +0430

    shahab twenty eighth push. 1- Camera Changes

--- app/src/main/java/ir/tdaapp/paymanyar/Presenter/P_MagnifierFragment.java
+++ app/src/main/java/ir/tdaapp/paymanyar/Presenter/P_MagnifierFragment.java
@@ -39,6 +39,7 @@ public class P_MagnifierFragment implements SurfaceHolder.Callback {
     android.hardware.Camera.ShutterCallback shutterCallback;
     android.hardware.Camera.PictureCallback jpegCallback;
     int zoom=1;
+    int S_width=10;int s_height=10;
 
     public P_MagnifierFragment(Context context, S_MagnifierFragment s_magnifierFragment) {
         this.context = context;
@@ -49,43 +50,53 @@ public class P_MagnifierFragment implements SurfaceHolder.Callback {
 
     public void Initial(SurfaceView surfaceView){
         surfaceHolder = surfaceView.getHolder();
-        surfaceHolder.addCallback(this);
         surfaceView.setFocusable(true);
-        surfaceView.requestFocus();
-        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-        rawCallback = new android.hardware.Camera.PictureCallback() {
-            public void onPictureTaken(byte[] data, Camera camera) {
-                Log.d("Log", "onPictureTaken - raw");
-            }
-        };
+        surfaceView.setFocusableInTouchMode(true);
+        s_height=surfaceView.getHeight();
+        S_width=surfaceView.getWidth();
 
-        /** Handles data for jpeg picture */
-        shutterCallback = new android.hardware.Camera.ShutterCallback() {
-            public void onShutter() {
-                Log.i("Log", "onShutter'd");
-            }
-        };
-        jpegCallback = new android.hardware.Camera.PictureCallback() {
-            public void onPictureTaken(byte[] data, Camera camera) {
-                FileOutputStream outStream = null;
-                try {
-                    outStream = new FileOutputStream(String.format(
-                            "/sdcard/DCIM/Camera/%d.jpg", System.currentTimeMillis()));
-                    outStream.write(data);
-                    outStream.close();
-                    Log.d("Log", "onPictureTaken - wrote bytes: " + data.length);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                }
-                Log.d("Log", "onPictureTaken - jpeg");
-                camera.startPreview();
-            }
-        };
+        Handler myHandler = new Handler();
+        myHandler.postDelayed(runnable,1000);
     }
 
+    Runnable runnable=new Runnable() {
+        @Override
+        public void run() {
+            surfaceHolder.addCallback(P_MagnifierFragment.this);
+            surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+            rawCallback = new android.hardware.Camera.PictureCallback() {
+                public void onPictureTaken(byte[] data, Camera camera) {
+                    Log.d("Log", "onPictureTaken - raw");
+                }
+            };
+
+            /** Handles data for jpeg picture */
+            shutterCallback = new android.hardware.Camera.ShutterCallback() {
+                public void onShutter() {
+                    Log.i("Log", "onShutter'd");
+                }
+            };
+            jpegCallback = new android.hardware.Camera.PictureCallback() {
+                public void onPictureTaken(byte[] data, Camera camera) {
+                    FileOutputStream outStream = null;
+                    try {
+                        outStream = new FileOutputStream(String.format(
+                                "/sdcard/DCIM/Camera/%d.jpg", System.currentTimeMillis()));
+                        outStream.write(data);
+                        outStream.close();
+                        Log.d("Log", "onPictureTaken - wrote bytes: " + data.length);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                    }
+                    Log.d("Log", "onPictureTaken - jpeg");
+                    camera.startPreview();
+                }
+            };
+        }
+    };
 
     public void Requestermission(){
 
@@ -115,15 +126,10 @@ public class P_MagnifierFragment implements SurfaceHolder.Callback {
             param = camera.getParameters();
             //modify parameter
             param.setPreviewFrameRate(20);
-            List<Camera.Size> sizes = param.getSupportedPictureSizes();
-            Camera.Size size = sizes.get(0);
-            for(int i=0;i<sizes.size();i++)
-            {
-                if(sizes.get(i).width > size.width)
-                    size = sizes.get(i);
-            }
-            param.setPictureSize(size.width, size.height);
-//            param.setPreviewSize(176, 220);
+            List<Camera.Size> sizes = param.getSupportedPreviewSizes();
+            Camera.Size optimalSize = getOptimalPreviewSize(sizes,S_width,s_height);
+            param.setPreviewSize(optimalSize.width,optimalSize.height);
+
             param.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
             ChangeZoom();
             camera.setParameters(param);
@@ -146,7 +152,38 @@ public class P_MagnifierFragment implements SurfaceHolder.Callback {
         camera.release();
     }
 
+    private Camera.Size getOptimalPreviewSize(List<Camera.Size> sizes, int w, int h) {
 
+        final double ASPECT_TOLERANCE = 0.1;
+        double targetRatio=(double)h / w;
+
+        if (sizes == null) return null;
+
+        Camera.Size optimalSize = null;
+        double minDiff = Double.MAX_VALUE;
+
+        int targetHeight = h;
+
+        for (Camera.Size size : sizes) {
+            double ratio = (double) size.width / size.height;
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Camera.Size size : sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+        return optimalSize;
+    }
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
         try{
@@ -156,8 +193,38 @@ public class P_MagnifierFragment implements SurfaceHolder.Callback {
     }
 
     @Override
-    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int w, int h) {
+        //before changing the application orientation, you need to stop the preview, rotate and then start it again
 
+        if(surfaceHolder.getSurface() == null)//check if the surface is ready to receive camera data
+            return;
+
+        try{
+            camera.stopPreview();
+        } catch (Exception e){
+            //this will happen when you are trying the camera if it's not running
+        }
+
+        //now, recreate the camera preview
+        try{
+            //set the focusable true
+            //this.setFocusable(true);
+            //set the touch able true
+            //this.setFocusableInTouchMode(true);
+            //set the camera display orientation lock
+            camera.setDisplayOrientation(90);
+
+            Camera.Parameters params = camera.getParameters();
+            List<Camera.Size> sizes = params.getSupportedPreviewSizes();
+            Camera.Size optimalSize = getOptimalPreviewSize(sizes,w,h);
+            params.setPreviewSize(optimalSize.width,optimalSize.height);
+            camera.setParameters(params);
+
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        } catch(Exception exp){
+            Log.i("Camera","FROM surfaceChanged: "+exp.toString());
+        }
     }
 
     @Override
